0aaa87ef7b18fd24ec31464a6ddb35005b9fe76c
Syenar <syenar@users.noreply.github.com>
2026-09-14 09:08:14 -0400

    Make RGB studio launcher wait for server

--- run-logo-rgb.cmd
+++ run-logo-rgb.cmd
@@ -3,5 +3,6 @@ setlocal
 set "APP_DIR=%~dp0"
 git -C "%APP_DIR%" pull --ff-only origin main
 if errorlevel 1 exit /b 1
+start "Mindaural server" cmd /c "cd /d ""%APP_DIR%"" && npm.cmd run serve"
+timeout /t 2 /nobreak >nul
 start "Mindaural RGB Logo Studio" "http://127.0.0.1:4173/logo-rgb.html"
-npm.cmd run serve
